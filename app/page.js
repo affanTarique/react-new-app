bec550114441dd42de2a274d9015ec0284797af2
affanTarique <reply2affankhan@gmail.com>
2025-07-28 23:39:36 +0530

    js

--- app/page.js
+++ app/page.js
@@ -3,10 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div> Hello kaise ho aap </div>
-    <div> Affan khan </div>
-    <div> hiii all  taimoor shamu dua kabir </div>
-    <div> hii arshu heloo hii temu hru </div>
-    <div>hiii baby react seekh raha hoo dean  hiii </div>
+    <div> Affan 
   );
 } ;
 
