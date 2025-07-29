626b6d99d6f9bfe3dc9e9207106d4136ddfe597c
affanTarique <reply2affankhan@gmail.com>
2025-07-29 23:54:40 +0530

    r

--- app/page.js
+++ app/page.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div> Hello kaise ho aap </div>
-    <div> Affan khan </div> 
-    <div> This is my first page </div>
-    <div> This is my first react app </div>
-  );
-} ;
+    <div> NEW N</div>
+    <div> new page </div>
+  )
+}
 
-export default page;
+export default page
