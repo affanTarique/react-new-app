025e36b75541b8cfb8409514bc5301977f13593d
affanTarique <reply2affankhan@gmail.com>
2025-07-31 21:33:46 +0530

    r

--- app/page.js
+++ app/page.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const page = () => {
   let marks = 79
-  const changeMarks = () =
+  const changeMarks = () =>
   return (
     <>
     <h1 className = 'font-bold text-xl'> My total marks were {marks} </h1>
     <button onClick= {()=> {
       marks = 40
-     }}className='bg-gray-500 px-5 py-2 rounded mt-5 text-white font-bold'> update</button>
+     }}className='bg-gray-500 px-5 py-2 rounded mt-5 text-white font-bold'> update </button>
     </>
   )
 }
