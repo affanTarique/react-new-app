54bd750d817c3396b2b5da9403af189709201942
affanTarique <reply2affankhan@gmail.com>
2025-07-31 21:34:21 +0530

    r

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import React from 'react'
 const page = () => {
   let marks = 79
   const changeMarks = () =>
+    {
+    
   return (
     <>
     <h1 className = 'font-bold text-xl'> My total marks were {marks} </h1>
